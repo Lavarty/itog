2123dd67bae06ce16020485e04c02ac6c886d244
Lavarty <115399445+Lavarty@users.noreply.github.com>
2024-05-03 20:31:18 +0700

    Update telebotishka.py

--- telebotishka.py
+++ telebotishka.py
@@ -5,21 +5,23 @@ bot = telebot.TeleBot("7094729383:AAFIvc_Vmk4Nwj1RMTZ2L2lUiC5H6rJ9MM8")
 @bot.message_handler(commands = ['start'])
 def start(message):
 
-    markup = types.InlineKeyboardMarkup()
+    markup = types.InlineKeyboardMarkup(resize_keyboard=True)
     item1 = types.InlineKeyboardButton("✨Начнём✨", callback_data = 'GG')
     item2 = types.InlineKeyboardButton('Поддержка📈',url = 'https://www.youtube.com/@Lavartyraider')
-
-    markup.row(item1,item2)
+    item3 = types.InlineKeyboardButton('🤑Донат🤑', url='https://www.donationalerts.com/r/lavartyyt_official')
+    markup.row(item1,item2,item3)
 
     bot.send_message(message.chat.id , 'Здравствуйте я бот для помощи в игре в стандофф 2',reply_markup=markup)
 @bot.callback_query_handler(func=lambda call : True)
 def callback_message(call):
     if call.data == 'GG':
-        markup = types.InlineKeyboardMarkup()
+        markup = types.InlineKeyboardMarkup(resize_keyboard=True)
         item1 = types.InlineKeyboardButton("Прострелы", callback_data='prostrel')
         item2 = types.InlineKeyboardButton('Расскиды', callback_data='rasskidi')
         item3 = types.InlineKeyboardButton('Ванвей смоки', callback_data='lifehack')
-        markup.row(item1, item2, item3)
+        item4 = types.InlineKeyboardButton('Подсад', callback_data='podsad')
+        item5 = types.InlineKeyboardButton('Назад🔙', callback_data='back')
+        markup.row(item1, item2, item3,item4,item5)
        # bot.edit_message_text(call.message.chat.id, call.message.message_id, 'Выбери тип вишки', reply_markup=markup)
         bot.send_message(call.message.chat.id, ' Выбери тип вишки',reply_markup=markup)
 
@@ -33,12 +35,22 @@ def callback_message(call):
 
 @bot.message_handler(commands = ['dast'])
 def second_level(call):
-        markup = types.InlineKeyboardMarkup()
+        markup = types.InlineKeyboardMarkup(resize_keyboard=True)
         item1 = types.InlineKeyboardButton("Прострелы", callback_data='lifehack')
         item2 = types.InlineKeyboardButton('Расскиды', callback_data='lifehack')
         item3 = types.InlineKeyboardButton('Ванвей смоки', callback_data='lifehack')
-        markup.row(item1, item2, item3)
+        item4 = types.InlineKeyboardButton('Подсад', callback_data='podsad')
+        item5 = types.InlineKeyboardButton('Назад🔙', callback_data='back')
+        markup.row(item1, item2, item3,item4,item5)
 
         bot.send_message(call.data.message.chat.id, 'Здравствуйте я бот для помощи в игре в стандофф 2',reply_markup=markup)
+        if call.data == "back":
+            markup = types.InlineKeyboardMarkup(resize_keyboard=True)
+            item1 = types.InlineKeyboardButton("✨Начнём✨", callback_data='GG')
+            item2 = types.InlineKeyboardButton('Поддержка📈', url='https://www.youtube.com/@Lavartyraider')
+            item3 = types.InlineKeyboardButton('🤑Донат🤑', url='https://www.donationalerts.com/r/lavartyyt_official')
+            markup.row(item1, item2, item3)
+
+            bot.send_message(message.chat.id, 'Здравствуйте я бот для помощи в игре в стандофф 2', reply_markup=markup)
 
 bot.polling(none_stop=True)
